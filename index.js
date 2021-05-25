71076eab616782e8008074fa9b01ec70a6f4efa3
SnoeProductions <>
2021-05-25 19:17:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let pluralNoun = "Jade's" ;
+let name = "Jade" ;
+let verb = "dance" ;
+let adjective = "passionate" ;
+let color = "green" ;
+
+console.log(`JavaScript provides a ${color} collection of tools — including 
+${adjective}  syntax and
+${pluralNoun}  — that allows ${name} to  ${verb}  with strings.`);
